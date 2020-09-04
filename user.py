2b734802d46f676994a70f06a99cf41199d44593
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-09-04 22:09:05 +0300

    added a user list

--- user.py
+++ user.py
@@ -1,4 +1,15 @@
-class user:
+class User:
     """
     Class that generates new instances of users
     """
+    user_list = []
+
+    def _init_(self,user_name,pasword):
+        self.user_name = user_name
+        self.pasword = pasword
+
+    def save_user(self):
+        """
+        save_user method saves the new users object
+        """
+        User.user_list.append(self)
